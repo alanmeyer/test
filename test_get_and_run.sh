b196e498ef00be24f6cd7b2265d3f662cc666c9d
alanmeyer <alanmeyer@yahoo.com>
2014-07-25 16:25:55 -0700

    Update

--- test_get_and_run.sh
+++ test_get_and_run.sh
@@ -5,12 +5,13 @@
 GIT_PROJ=alanmeyer/test/master
 GIT_SCRIPT=test
 
-rm -f $GIT_SCRIPT"_get_and_run.sh"
-rm -f $GIT_SCRIPT.py
-rm -f $GIT_SCRIPT.cfg
-wget https://raw.github.com/$GIT_PROJ/$GIT_SCRIPT"_get_and_run.sh"
-wget https://raw.github.com/$GIT_PROJ/$GIT_SCRIPT".py"
+GIT_SCRIPT_GET=$GIT_SCRIPT"_get_and_run.sh"
+GIT_SCRIPT_PYT=$GIT_SCRIPT".py"
+GIT_SCRIPT_CFG=$GIT_SCRIPT".cfg"
 
+rm -f $GIT_SCRIPT_GET $GIT_SCRIPT_PYT $GIT_SCRIPT_CFG
+wget https://raw.github.com/$GIT_PROJ/$GIT_SCRIPT_GET
+wget https://raw.github.com/$GIT_PROJ/$GIT_SCRIPT_PYT
 chmod +x *.sh
 
-python $GIT_SCRIPT.py
+python $GIT_SCRIPT_PYT
